30a20dfbe90849be00005776ab48da333f75ae14
meirelon <nestelm@gmail.com>
2019-02-25 21:54:52 -0500

    int

--- image_detection.py
+++ image_detection.py
@@ -58,7 +58,7 @@ def run_quickstart(photo_link, detection_type="label", from_internet=True):
                              "surprised":face.surprise_likelihood}
         emotion_list = []
         for k,v in emotion_dictionary.items():
-            if v.lower() == "very_likely":
+            if v == "VERY_LIKELY":
                 emotion_list.append(k)
         if len(emotion_list) > 0:
             print(emotion_list[0])
