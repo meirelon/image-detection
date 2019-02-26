880b49c8da118bd8b09ca6a2471f9e837ceb50d6
meirelon <nestelm@gmail.com>
2019-02-25 21:57:34 -0500

    pri

--- image_detection.py
+++ image_detection.py
@@ -61,6 +61,7 @@ def run_quickstart(photo_link, detection_type="label", from_internet=True):
             if v == 5:
                 emotion_list.append(k)
         if len(emotion_list) > 0:
+            print(emotion_list[0])
             return emotion_list[0]
 
 
